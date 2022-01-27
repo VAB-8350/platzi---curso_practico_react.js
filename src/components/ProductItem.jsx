dcbd7284b3a33cfd23e3e25b8711f2b61589ee1e
vabklb <v.barilin@itecriocuarto.org.ar>
2022-01-27 18:19:00 -0300

    add new files

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/ProductItem.scss';
+import '@styles/ProductItem.scss';
+import addCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
 	return (
@@ -11,7 +12,7 @@ const ProductItem = () => {
 					<p>Bike</p>
 				</div>
 				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+					<img src={addCart} alt="" />
 				</figure>
 			</div>
 		</div>
