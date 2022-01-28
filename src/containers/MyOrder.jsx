0131b11a7809f465e085af3a64b752c69812fee3
vabklb <v.barilin@itecriocuarto.org.ar>
2022-01-27 21:16:17 -0300

    add new files

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import OrderItem from '@components/OrderItem';
+import AppContext from '@context/AppContext';
 import '@styles/MyOrder.scss';
 import flechita from '@icons/flechita.svg';
 
 const MyOrder = () => {
+	const { state } = useContext(AppContext);
+
+	const sumTotal = () => {
+		const reducer = (acumulator, currentValue) => acumulator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	}
+
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -11,12 +20,14 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(item => (
+					<OrderItem product={item} key={`orderItem-${item.id}`} />
+				))}				
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
